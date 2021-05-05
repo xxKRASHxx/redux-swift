7542419257ab26b65e81aa7cfb8e9a17fe926ee7
Lisovoy, Danil <danil.lisovoy@sigma.software>
2021-05-05 13:28:02 +0300

    first commit

---
+++ ReduxDemo/ReduxDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxDemoApp.swift
+//  ReduxDemo
+//
+//  Created by Daniel Lisovoy on 30.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReduxDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReduxDemo/View/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ReduxDemo
+//
+//  Created by Daniel Lisovoy on 30.04.2021.
+//
+
+import SwiftUI
+import Redux
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
