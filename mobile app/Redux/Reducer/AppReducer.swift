8d424f51bdf91f6b14e10a322b485150b8b35a12
Lisovoy, Danil <danil.lisovoy@sigma.software>
2021-05-07 00:35:41 +0300

    make it private

--- mobile app/Redux/Reducer/AppReducer.swift
+++ mobile app/Redux/Reducer/AppReducer.swift
@@ -2,7 +2,7 @@ import Redux
 
 typealias AppReducer = Reducer<AppState, AppAction>
 
-let counter = PartialReducer<AppState, AppAction>(of: \.counter, counterReducer)
+private let counter = PartialReducer<AppState, AppAction>(of: \.counter, counterReducer)
 
 let appReducer: AppReducer = CompositeReducer<AppState, AppAction>(
     counter.reduce
